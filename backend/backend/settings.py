8b63c938179e2e7812c2922440c15a33b43b685d
lonlait <nbespalova@gmail.com>
2025-06-11 07:59:21 +0000

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,13 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_key_for_tests')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.70.136', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['gopto.hopto.org', '158.160.70.136', '127.0.0.1', 'localhost']
 
 
 # Application definition
